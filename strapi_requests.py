f98d95b0cec78ca3cf9a7e4364e522f7506aebe0
ksyusha123 <72920390+ksyusha123@users.noreply.github.com>
2022-12-28 21:33:50 +0500

    Update strapi_requests.py

--- strapi_requests.py
+++ strapi_requests.py
@@ -8,15 +8,8 @@ url = 'http://51.178.191.76:1337'
 def post_new_dtp_object(bearer: str, data: str) -> dict:
     headers = dict([
         ('Accept', 'application/json'),
-        ('Accept-Language', 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7'),
         ('Authorization', f'Bearer {bearer}'),
-        ('Cache-Control', 'no-cache'),
-        ('Connection', 'keep-alive'),
-        ('Content-Type', 'application/json'),
-        ('Origin', 'http://51.178.191.76:1337'),
-        ('Pragma', 'no-cache'),
-        ('User-Agent',
-         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56')
+        ('Content-Type', 'application/json')
     ])
     response = requests.post(f'http://51.178.191.76:1337/api/dtps', json={"data": data},
                              headers=headers)
